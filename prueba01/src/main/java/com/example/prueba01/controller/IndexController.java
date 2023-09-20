5b96e4fcb72a7dd317fc465beedca3223b1069d1
Your Name <you@example.com>
2023-09-20 10:33:06 -0500

    tabla

--- prueba01/src/main/java/com/example/prueba01/controller/IndexController.java
+++ prueba01/src/main/java/com/example/prueba01/controller/IndexController.java
@@ -3,9 +3,13 @@ package com.example.prueba01.controller;
 import com.example.prueba01.dto.ClientDto;
 import com.example.prueba01.service.impl.ClientServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -20,5 +24,15 @@ public class IndexController {
         List<ClientDto> usuarios = clientService.getAllClients();
         model.addAttribute("clientes", usuarios);
         return "clientes"; // El nombre de la vista HTML
+    }
+
+    @PostMapping("/crearCliente")
+    public String createClient(@RequestBody ClientDto clientDto) {
+        try {
+            clientService.createClient(clientDto);
+            return "redirect:/";
+        } catch (Exception e) {
+            return "redirect:/error";
+        }
     }
 }
